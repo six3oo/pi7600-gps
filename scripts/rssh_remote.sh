925c3c0b984b6f3d676c3a0f8b920e577a02ca94
Daazed McFarland <daazedjmcfarland@gmail.com>
2025-01-08 03:03:47 -0800

    update rssh_remote.sh

--- scripts/rssh_remote.sh
+++ scripts/rssh_remote.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Fill in <> with your applicable data. This will start a ssh session that you can then reverse ssh on.
-sshpass -p <password> ssh -b <yourdeviceipaddr> -R <portforrssh>:localhost:22 <yourusername>@<yourip> -p <youropenport>
+sshpass -p <Password> ssh -fN -R <LocalHostPort>:localhost:22 <User>@<ConnectionAddress> -p <RouterAccessPort>
 exit
